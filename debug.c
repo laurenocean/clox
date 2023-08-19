3e859d1b4ab4f952074bc5b49b1c3dedc6306426
Lauren Ocean <lauren.ocean@me.com>
2023-08-19 10:44:31 -0400

    Chapter 29

--- debug.c
+++ debug.c
@@ -69,6 +69,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_SET_UPVALUE:    return byteInstruction("OP_SET_UPVALUE", chunk, offset);
         case OP_GET_PROPERTY:   return constantInstruction("OP_GET_PROPERTY", chunk, offset);
         case OP_SET_PROPERTY:   return constantInstruction("OP_SET_PROPERTY", chunk, offset);
+        case OP_GET_SUPER:      return constantInstruction("OP_GET_SUPER", chunk, offset);
         case OP_EQUAL:          return simpleInstruction("OP_EQUAL", offset);
         case OP_GREATER:        return simpleInstruction("OP_GREATER", offset);
         case OP_LESS:           return simpleInstruction("OP_LESS", offset);
@@ -84,6 +85,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_LOOP:           return jumpInstruction("OP_LOOP", -1, chunk, offset);
         case OP_CALL:           return byteInstruction("OP_CALL", chunk, offset);
         case OP_INVOKE:         return invokeInstruction("OP_INVOKE", chunk, offset);
+        case OP_SUPER_INVOKE:   return invokeInstruction("OP_SUPER_INVOKE", chunk, offset);
         case OP_CLOSURE: {
             offset++;
             uint8_t constant = chunk->code[offset++];
@@ -106,7 +108,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         case OP_CLASS:          
-            return constantInstruction("OP_CLASS", chunk, offset);
+            return constantInstruction("OP_CLASS", chunk, offset);\
+        case OP_INHERIT:        return simpleInstruction("OP_INHERIT", offset);
         case OP_METHOD: 
             return constantInstruction("OP_METHOD", chunk, offset);
         default:
